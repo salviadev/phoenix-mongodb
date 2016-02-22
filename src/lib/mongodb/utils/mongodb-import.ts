bf7303e61d7d3b147c16483542cef25cf9a8f0f9
misto <mihaistoie@gmail.com>
2016-02-22 17:54:07 +0100

    #

--- src/lib/mongodb/utils/mongodb-import.ts
+++ src/lib/mongodb/utils/mongodb-import.ts
@@ -9,30 +9,37 @@ var csv = require('fast-csv');
 
 
 
-export function importCollectionFromStream(db: mongodb.Db,  collection: mongodb.Collection, schema: any, stream: stream.Readable, options?: {truncate: boolean, onImported: any, format?:string}, tenantId?: number): Promise<number> {
-        options = options || {truncate: true, onImported: null};
-        let isCsv = options.format === 'csv';
-        return new Promise<number>((resolve, reject) => {
+export function importCollectionFromStream(db: mongodb.Db, collection: mongodb.Collection, schema: any, stream: stream.Readable, options?: { truncate: boolean, onImported: any, format?: string }, tenantId?: number): Promise<number> {
+    options = options || { truncate: true, onImported: null };
+    let isCsv = options.format === 'csv';
+    return new Promise<number>((resolve, reject) => {
 
-            let handleError = function(err): void {
-                reject(err);
-            };
-            try {
-                let ms = new mongoStream.MongoDbWriteStream(schema, options.truncate, tenantId || 0, db, collection);
-                let parser = isCsv ? csv({delimiter: ';'}): JSONStream.parse('*');
-                ms = stream.pipe(parser).pipe(ms);
-                stream.on('error', handleError);
-                stream.on('error', handleError);
-                parser.on('error', handleError);
-                ms.on('error', handleError);
-                ms.on('finish', function() {
-                    if (options.onImported)
-                        options.onImported(schema, ms.count);
-                    resolve(ms.count);
-                });
-            } catch (ex) {
-                handleError(ex);
+        let handleError = function(err): void {
+            reject(err);
+        };
+        try {
+            let ms = new mongoStream.MongoDbWriteStream(schema, options.truncate, tenantId || 0, db, collection);
+            let parser = isCsv ? csv({ delimiter: ';' }) : JSONStream.parse('*');
+            if (idCsv) {
+                 parser.on("data", function(data) {
+                     console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+                    console.log(data);
+                })
             }
-        });
-    }
+            ms = stream.pipe(parser).pipe(ms);
+            stream.on('error', handleError);
+            stream.on('error', handleError);
+            parser.on('error', handleError);
+            ms.on('error', handleError);
+            ms.on('finish', function() {
+                if (options.onImported)
+                    options.onImported(schema, ms.count);
+                resolve(ms.count);
+            });
+        } catch (ex) {
+            handleError(ex);
+        }
+    });
+}
 
